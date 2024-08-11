d56387f8f43fde8674583367b58fc640a9e13a67
Atharva Goliwar <atharva.goliwar23@gmail.com>
2024-08-11 15:48:32 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ connectDB();
 // }
 
 app.use(cors({
-    origin: 'http://localhost:5173', // replace with your React app's URL
+    origin: 'https://doc-it-up.vercel.app/', // replace with your React app's URL
 }));
 
 const conn = mongoose.connection;
